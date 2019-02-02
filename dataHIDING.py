d859786c7ec98df72c9cd389947246d09ce2e748
ANIKET RAI <prosniperaniket@gmail.com>
2019-02-02 08:28:28 +0530

    Update dataHIDING.py
    Added tkinter directory selection to get the folder you want to encrypt

--- dataHIDING.py
+++ dataHIDING.py
@@ -1,20 +1,27 @@
 import os
 import shutil
+from tkinter import filedialog
+from tkinter import *
+
 
 
 def zunz(args):
-        if(args==2):
-                print("Unzipping")
-                os.system("unzip final2.jpg")
-        else:
+    if(args==2):
+        print("Unzipping")
+        os.system("unzip final2.jpg")
  #root_dir = directory of your folder containing secret files
-                shutil.make_archive('final','zip',root_dir='/home/aniket/Desktop/Test/Python/Python Project/image hiding/FINAL/DATA',base_dir=None)
-                print("zip created sucessfully")
-                os.system("cat final.jpg final.zip > final2.jpg") 
+ #d='/home/aniket/Desktop/Test/Python/Python Project/image hiding/FINAL/DATA'
+    else:
+        root=Tk() #Tk window to hold the button:
+        root.update()
+        shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(),base_dir=None)
+        root.destroy()
+        print("zip created sucessfully")
+        os.system("cat final.jpg final.zip > final2.jpg") 
                 
 
 print("1. Make Zip \n2.Unzip") 
-if(int(input())==0):
+if(int(input())==1):
     zunz(1)
 else:
     zunz(2)
