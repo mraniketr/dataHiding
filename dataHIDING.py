5682c4200c87cbb2d13fa473ccfbd683e3c24291
ANIKET RAI <prosniperaniket@gmail.com>
2019-02-02 07:23:54 +0530

    Update dataHIDING.py
    Added a command line menu.

--- dataHIDING.py
+++ dataHIDING.py
@@ -1,12 +1,20 @@
 import os
 import shutil
 
-#root_dir = directory of your folder containing secret files
-shutil.make_archive('final','zip',root_dir='/home/aniket/Desktop/Test/Python/Python Project/image hiding/FINAL/DATA',base_dir=None)
 
-print("zip created sucessfully")
+def zunz(args):
+        if(args==2):
+                print("Unzipping")
+                os.system("unzip final2.jpg")
+        else:
+ #root_dir = directory of your folder containing secret files
+                shutil.make_archive('final','zip',root_dir='/home/aniket/Desktop/Test/Python/Python Project/image hiding/FINAL/DATA',base_dir=None)
+                print("zip created sucessfully")
+                os.system("cat final.jpg final.zip > final2.jpg") 
+                
 
-os.system("cat final.jpg final.zip > final.jpg")
-
-print("Unzipping")
-os.system("unzip final.jpg")
+print("1. Make Zip \n2.Unzip") 
+if(int(input())==0):
+    zunz(1)
+else:
+    zunz(2)
