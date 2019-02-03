f15955687189d4c03d151ab5a95895484d8b6773
ANIKET RAI <prosniperaniket@gmail.com>
2019-02-03 17:02:30 +0530

    Update dataHIDING.py
    Now working with windows , linux and mac.

--- dataHIDING.py
+++ dataHIDING.py
@@ -1,20 +1,33 @@
-import os
-import shutil
+import os #to run system commands
+import shutil  #to make archive
 from tkinter import filedialog
 from tkinter import *
+import sys #to know the os
 
 def zunz(args):
     if(args==2):
-        print("Unzipping")
-        os.system("unzip final2.jpg")
+        if(sys.platform=='win32'):
+            os.system('rename final2.jpg final2.zip')
+            os.system('unzip final2.zip')
+        else:
+            os.system("unzip final2.jpg") 
+       	print("Unzipping sucessful")
+   
     elif(args==1):
-        root=Tk() #Tk window to hold the button:
+       #keep a image of file name final.jpg
+        root=Tk()
         root.update()
         shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(title = "Open directory containg your files"),base_dir=None)
         root.destroy()
+        if(sys.platform=='win32'):
+            os.system("copy /b final.jpg+final.zip final2.jpg")
+            os.system("del final.zip")
+        else:
+            os.system("cat final.jpg final.zip > final2.jpg") #to create .jpg with our data
+            os.system("rm final.zip") #delete the .zip file 
+       
         print("zip created sucessfully")
-        os.system("cat final.jpg final.zip > final2.jpg") 
-        os.system("rm final.zip") 
+
     else:
         print("Invalid IP")
                 
