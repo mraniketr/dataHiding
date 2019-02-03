a192e45162626c2148029c123216d31172d18df1
ANIKET RAI <prosniperaniket@gmail.com>
2019-02-03 17:17:33 +0530

    Update dataHIDING.py
    Code formatting

--- dataHIDING.py
+++ dataHIDING.py
@@ -1,38 +1,41 @@
-import os #to run system commands
-import shutil  #to make archive
+import os 															#to run system commands
+import shutil 			 										    #to make archive
 from tkinter import filedialog
 from tkinter import *
-import sys #to know the os
+import sys 															#to know the os for os specific commands
 
 def zunz(args):
-    if(args==2):
+    if(args==2):													#Extraction
         if(sys.platform=='win32'):
-            os.system('rename final2.jpg final2.zip')
-            os.system('unzip final2.zip')
+            os.system('rename classified.jpg classified.zip')
+            os.system('unzip classified.zip')
         else:
-            os.system("unzip final2.jpg") 
-       	print("Unzipping sucessful")
+            os.system("unzip classified.jpg") 
+       	print("Extraction successful")
    
-    elif(args==1):
-       #keep a image of file name final.jpg
+    elif(args==1):													#Encryption
+
+   
         root=Tk()
         root.update()
+   																	#Creating Zip
         shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(title = "Open directory containg your files"),base_dir=None)
         root.destroy()
-        if(sys.platform=='win32'):
-            os.system("copy /b final.jpg+final.zip final2.jpg")
-            os.system("del final.zip")
+
+        if(sys.platform=='win32'):			
+            os.system("copy /b final.jpg+final.zip classified.jpg")		#to create encrypted image
+            os.system("del final.zip")								#delete the .zip file
         else:
-            os.system("cat final.jpg final.zip > final2.jpg") #to create .jpg with our data
-            os.system("rm final.zip") #delete the .zip file 
+            os.system("cat final.jpg final.zip > classified.jpg") 		#to create encrypted image
+            os.system("rm final.zip") 								#delete the .zip file 
        
-        print("zip created sucessfully")
+        print("Encryption successful")
 
     else:
-        print("Invalid IP")
+        print("Invalid Input")
                 
 
-print("1. Make Zip \n2.Unzip") 
+print("\n1. Create classified image \n2. Extract Data \n") 
 if(int(input())==1):
     zunz(1)
 else:
