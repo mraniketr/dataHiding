60c08fc99450eee5737ea1ece0fc51c900b1061d
Aniket-PC <prosniperaniket@gmail.com>
2019-03-24 01:57:33 +0530

    minor fixes

--- dataHIDING.py
+++ dataHIDING.py
@@ -3,41 +3,43 @@ import os
 #to make archive
 import shutil
 #Filedialog for selection
-from tkinter import filedialog
 from tkinter import *
+from tkinter import filedialog
 #to distinguish between OS
 import sys
 
+
 #function to unzip the encrypted image with classified information
 def fuzip():
-	if(sys.platform=='win32'):
-		os.system('rename classified.jpg classified.zip')
-		os.system('unzip classified.zip')
-	else:
-		os.system("unzip classified.jpg") 
-	print("Extraction successful")
+    if(sys.platform=='win32'):
+        os.system('rename classified.jpg classified.rar')
+        os.system('7z.exe x classified.rar')
+    else:
+        os.system("unzip classified.jpg")
+    print("Extraction successful")
 
 #function to create zip and the encrypted image with classified information   
 def fzip():
-	root=Tk()
-	root.update()
-	shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(title = "Open directory containg your files"),base_dir=None)
-	root.destroy()
+    root=Tk()
+    root.update()
+    shutil.make_archive('final','zip',root_dir=filedialog.askdirectory(title = "Open directory containg your files"),base_dir=None)
+    root.destroy()
 
-	if(sys.platform=='win32'):
-		isrc=filedialog.askopenfilename()
-		os.rename(isrc,'final.jpg')
-		os.system("copy /b final.jpg+final.zip classified.jpg")
-		os.system("del final.zip")
-	
-	else:
-		isrc=filedialog.askopenfilename()
-		os.rename(isrc,'final.jpg')
-		os.system("cat final.jpg final.zip > classified.jpg")
-		os.system("rm final.zip")
-	print("Encryption successful")                
+    if(sys.platform=='win32'):
+        isrc=filedialog.askopenfilename()
+        os.rename(isrc,'final.jpg')
+        os.system("copy /b final.jpg+final.zip classified.jpg")
+        os.system("del final.zip")
+        os.system("del final.jpg")
 
-print("\n1. Create classified image \n2. Extract Data \n") 
+    else:
+        isrc=filedialog.askopenfilename()
+        os.rename(isrc,'final.jpg')
+        os.system("cat final.jpg final.zip > classified.jpg")
+        os.system("rm final.zip")
+    print("Encryption successful")
+
+print("\n1. Create classified image \n2. Extract Data \n")
 if(int(input())==1):
     fzip()
 else:
